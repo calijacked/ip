619c6b89bb5b92c8c6ca84d6beb9c254d6cfd27d
calijacked <162989170+calijacked@users.noreply.github.com>
2026-01-22 19:49:36 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Ragebait.java
+++ src/main/java/Ragebait.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 public class Ragebait {
     public static void main(String[] args) {
@@ -6,13 +7,15 @@ public class Ragebait {
                         "Hello Fatty! I'm Ragebait! What can I do for you? :)\n" +
                         "_________________________________________");
         Scanner sc = new Scanner(System.in);
-
+        ArrayList<String> userText = new ArrayList<>();
         while (true) {
             String input = sc.nextLine().trim().toLowerCase();
             System.out.println("____________________________________________________________");
             switch (input) {
                 case "list":
-                    System.out.println("here you go! an empty list!");
+                    for(int i = 0; i < userText.size(); i++) {
+                        System.out.println(String.valueOf(i+1) + ". " + userText.get(i));
+                    }
                     break;
                 case "blah":
                     System.out.println("Blah blah blah… wow, you’re original!");
@@ -20,9 +23,11 @@ public class Ragebait {
                 case "bye":
                     System.out.println("Finally! I was getting tired of you!");
                     System.out.println("____________________________________________________________");
-
                     sc.close();
                     return;
+                default:
+                    userText.add(input);
+                    System.out.println("added: " + input);
             }
             System.out.println("____________________________________________________________");
         }
