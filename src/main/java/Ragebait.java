e89a7bc5dbef0d4acc7dca4e8e7780ecde9e6c6e
calijacked <162989170+calijacked@users.noreply.github.com>
2026-01-22 18:16:05 +0800

    Add basic Ragebait chatbot user commands: responds to 'list', 'blah' and 'bye'

--- src/main/java/Ragebait.java
+++ src/main/java/Ragebait.java
@@ -1,8 +1,30 @@
+import java.util.*;
 public class Ragebait {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Ragebait! What can I do for you?\n" +
-                        "____________________________________________________________\n" +
-                        "Bye. Hope to see you again soon!\n" +
-                "_________________________________________");
+        System.out.println(
+                "____________________________________________________________\n" +
+                        "Hello Fatty! I'm Ragebait! What can I do for you? :)\n" +
+                        "_________________________________________");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine().trim().toLowerCase();
+            System.out.println("____________________________________________________________");
+            switch (input) {
+                case "list":
+                    System.out.println("here you go! an empty list!");
+                    break;
+                case "blah":
+                    System.out.println("Blah blah blah… wow, you’re original!");
+                    break;
+                case "bye":
+                    System.out.println("Finally! I was getting tired of you!");
+                    System.out.println("____________________________________________________________");
+
+                    sc.close();
+                    return;
+            }
+            System.out.println("____________________________________________________________");
+        }
     }
 }
