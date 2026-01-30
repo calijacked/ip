f240c98b49a39898b80dd3a2dad678436626bd0c
calijacked <162989170+calijacked@users.noreply.github.com>
2026-01-30 18:00:24 +0800

    Add JavaDoc for UnmarkCommand class

--- src/main/java/ragebait/command/UnmarkCommand.java
+++ src/main/java/ragebait/command/UnmarkCommand.java
@@ -4,17 +4,38 @@ import ragebait.storage.Storage;
 import ragebait.ui.UI;
 import ragebait.task.TaskList;
 
+/**
+ * Command to mark a task as not done in the TaskList.
+ * Used when the user wants to undo the completion of a task.
+ */
 public class UnmarkCommand extends Command {
     private final int index;
 
+    /**
+     * Constructs an UnmarkCommand for the task at the specified index.
+     *
+     * @param index Index of the task to unmark (0-based).
+     */
     public UnmarkCommand(int index) {
         this.index = index;
     }
 
+    /**
+     * Returns the index of the task to unmark.
+     *
+     * @return Task index.
+     */
     public int getIndex() {
         return index;
     }
 
+    /**
+     * Marks the task at the given index as not done and displays a message.
+     *
+     * @param tasks TaskList containing all tasks.
+     * @param ui UI used to display messages.
+     * @param storage Storage for saving data (not used in this command).
+     */
     @Override
     public void execute(TaskList tasks, UI ui, Storage storage) {
         if (index < 0 || index >= tasks.size()) {
