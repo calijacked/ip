7d1621bb0f3a1fd861c4aaae26a0f9a3728ddad5
calijacked <162989170+calijacked@users.noreply.github.com>
2026-01-30 18:00:12 +0800

    Add JavaDoc for abstract Command class

--- src/main/java/ragebait/command/Command.java
+++ src/main/java/ragebait/command/Command.java
@@ -4,7 +4,27 @@ import ragebait.storage.Storage;
 import ragebait.task.TaskList;
 import ragebait.ui.UI;
 
+/**
+ * Abstract class representing a user command in the Ragebait application.
+ * All commands should extend this class and implement the execute method.
+ */
 public abstract class Command {
+
+    /**
+     * Executes the command on the given tasks using UI and storage.
+     *
+     * @param tasks TaskList to operate on.
+     * @param ui    UI for user interaction.
+     * @param storage Storage for persisting task data.
+     */
     public abstract void execute(TaskList tasks, UI ui, Storage storage);
-    public boolean isExit() { return false; }
+
+    /**
+     * Indicates whether this command causes the application to exit.
+     *
+     * @return true if the command exits the program; false otherwise.
+     */
+    public boolean isExit() {
+        return false;
+    }
 }
