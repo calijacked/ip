9ada9d0da07d9eeef4448baef529ce90cb716eb8
calijacked <162989170+calijacked@users.noreply.github.com>
2026-01-30 18:00:19 +0800

    Add JavaDoc for ListCommand class

--- src/main/java/ragebait/command/ListCommand.java
+++ src/main/java/ragebait/command/ListCommand.java
@@ -4,7 +4,18 @@ import ragebait.storage.Storage;
 import ragebait.task.TaskList;
 import ragebait.ui.UI;
 
+/**
+ * Command to list all tasks currently stored in the TaskList.
+ */
 public class ListCommand extends Command {
+
+    /**
+     * Displays all tasks in the TaskList using the UI.
+     *
+     * @param tasks TaskList containing all tasks.
+     * @param ui UI used to display the list.
+     * @param storage Storage (not used for this command).
+     */
     @Override
     public void execute(TaskList tasks, UI ui, Storage storage) {
         ui.showMessage(tasks.listTasks());
