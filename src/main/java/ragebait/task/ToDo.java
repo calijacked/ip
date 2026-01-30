567a115486ec0db164fe941ee3eda3ab4d37b1d3
calijacked <162989170+calijacked@users.noreply.github.com>
2026-01-30 18:00:48 +0800

    Add JavaDoc for ToDo class

--- src/main/java/ragebait/task/ToDo.java
+++ src/main/java/ragebait/task/ToDo.java
@@ -1,11 +1,28 @@
 package ragebait.task;
 
+/**
+ * Represents a To-Do task in the Ragebait application.
+ * A To-Do task only has a description and a completion status,
+ * and does not require any date or time.
+ */
 public class ToDo extends Task {
 
+    /**
+     * Constructs a new To-Do task with the given description.
+     * The task is initially not completed.
+     *
+     * @param description Description of the To-Do task.
+     */
     public ToDo(String description) {
         super(description, TaskType.TODO);
     }
 
+    /**
+     * Constructs a To-Do task with a given description and completion status.
+     *
+     * @param description Description of the To-Do task.
+     * @param isDone Whether the task is completed.
+     */
     public ToDo(String description, boolean isDone) {
         super(description, TaskType.TODO);
         if (isDone) {
@@ -13,12 +30,21 @@ public class ToDo extends Task {
         }
     }
 
-
+    /**
+     * Returns a string representation of the To-Do task for display purposes.
+     *
+     * @return String representation of the task.
+     */
     @Override
     public String toString() {
         return super.toString();
     }
 
+    /**
+     * Returns a string suitable for saving the To-Do task to a file.
+     *
+     * @return File-format string of the To-Do task.
+     */
     @Override
     public String toFileFormat() {
         return "T | " + getStatusIcon() + " | " + description;
