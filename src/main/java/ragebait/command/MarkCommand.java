a8013df5263604187812b586ebe04e3481eb02ca
calijacked <162989170+calijacked@users.noreply.github.com>
2026-01-30 18:00:21 +0800

    Add JavaDoc for MarkCommand class

--- src/main/java/ragebait/command/MarkCommand.java
+++ src/main/java/ragebait/command/MarkCommand.java
@@ -4,16 +4,28 @@ import ragebait.storage.Storage;
 import ragebait.task.TaskList;
 import ragebait.ui.UI;
 
+/**
+ * Command to mark a task as completed in the TaskList.
+ */
 public class MarkCommand extends Command {
     private final int index;
 
+    /**
+     * Constructs a MarkCommand for the task at the specified index.
+     *
+     * @param index Index of the task to mark (0-based).
+     */
     public MarkCommand(int index) {
         this.index = index;
     }
 
-    public int getIndex() {
-        return index;
-    }
+    /**
+     * Marks the task at the given index as done and displays a message.
+     *
+     * @param tasks TaskList containing tasks.
+     * @param ui UI for displaying messages.
+     * @param storage Storage (not used for this command).
+     */
     @Override
     public void execute(TaskList tasks, UI ui, Storage storage) {
         if (index < 0 || index >= tasks.size()) {
@@ -23,5 +35,14 @@ public class MarkCommand extends Command {
         tasks.get(index).markDone();
         ui.showMessage("NICE LA! You managed to accomplish something in your life for once.");
         ui.showMessage(tasks.get(index).toString());
+    }
+
+    /**
+     * Returns the index of the task to mark.
+     *
+     * @return Task index.
+     */
+    public int getIndex() {
+        return index;
     }
 }
