4960455451fb00f3008103ed59764deca8db42a5
calijacked <162989170+calijacked@users.noreply.github.com>
2026-02-23 20:09:06 +0800

    Package JavaFX files

--- src/main/java/ragebait/Main.java
+++ src/main/java/ragebait/Main.java
@@ -1,3 +1,4 @@
+package ragebait;
 import java.io.IOException;
 
 import javafx.application.Application;
--- src/main/java/ragebait/ui/DialogBox.java
+++ src/main/java/ragebait/ui/DialogBox.java
@@ -1,3 +1,5 @@
+package ragebait.ui;
+
 import java.io.IOException;
 import java.util.Collections;
 
