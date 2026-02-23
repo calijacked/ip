544e774d8bfcb81e244f435cc46bcfaac8cbfec5
calijacked <162989170+calijacked@users.noreply.github.com>
2026-02-23 17:56:25 +0800

    Modify JavaDocs for Ragebait FindCommand Class

--- src/main/java/ragebait/command/FindCommand.java
+++ src/main/java/ragebait/command/FindCommand.java
@@ -5,23 +5,32 @@ import ragebait.task.Task;
 import ragebait.task.TaskList;
 import ragebait.ui.UI;
 
-
 /**
- * Finds and lists all tasks whose description contains the given keyword.
+ * Represents a command that searches for tasks containing
+ * a specified keyword in their description.
  */
 public class FindCommand extends Command {
 
+    /** The keyword used to search task descriptions. */
     private final String keyword;
 
     /**
-     * Constructs a FindCommand with the specified keyword.
+     * Creates a FindCommand with the specified keyword.
      *
-     * @param keyword Keyword to search in task descriptions.
+     * @param keyword The keyword to search for.
      */
     public FindCommand(String keyword) {
         this.keyword = keyword;
     }
 
+    /**
+     * Executes the find command by filtering tasks that contain
+     * the specified keyword and displaying the matching results.
+     *
+     * @param tasks The TaskList containing all tasks.
+     * @param ui The UI used to display messages.
+     * @param storage The Storage responsible for persisting tasks.
+     */
     @Override
     public void execute(TaskList tasks, UI ui, Storage storage) {
         TaskList matchingTasks = new TaskList();
@@ -38,5 +47,3 @@ public class FindCommand extends Command {
         }
     }
 }
-
-
