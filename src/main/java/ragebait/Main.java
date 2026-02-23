8c0f75908bb506e1c5dc1f262cf629d6339af735
calijacked <162989170+calijacked@users.noreply.github.com>
2026-02-24 00:42:57 +0800

    Update AddCommand

--- src/main/java/ragebait/Main.java
+++ src/main/java/ragebait/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Ragebait Chatbot");
             fxmlLoader.<MainWindow>getController().setRagebait(ragebait); // inject the Ragebait instance
             stage.show();
         } catch (IOException e) {
