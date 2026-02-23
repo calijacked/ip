dfc7adbd854c67f262349e22598c7dbb90d0f08b
calijacked <162989170+calijacked@users.noreply.github.com>
2026-02-23 17:54:05 +0800

    Modify JavaDocs for Ragebait CommandType Class

--- src/main/java/ragebait/command/CommandType.java
+++ src/main/java/ragebait/command/CommandType.java
@@ -2,17 +2,46 @@ package ragebait.command;
 
 import ragebait.exception.RagebaitException;
 
+/**
+ * Represents the different types of commands supported
+ * by the Ragebait application.
+ */
 public enum CommandType {
+
+    /** Lists all tasks. */
     list,
+
+    /** Exits the application. */
     bye,
+
+    /** Deletes a task. */
     delete,
+
+    /** Marks a task as completed. */
     mark,
+
+    /** Unmarks a completed task. */
     unmark,
+
+    /** Creates a todo task. */
     todo,
+
+    /** Creates a deadline task. */
     deadline,
+
+    /** Creates an event task. */
     event,
+
+    /** Finds tasks containing a keyword. */
     find;
 
+    /**
+     * Converts a string input into the corresponding CommandType.
+     *
+     * @param input The command word entered by the user.
+     * @return The matching CommandType.
+     * @throws RagebaitException If the input does not match any command type.
+     */
     public static CommandType convertToCommandType(String input) throws RagebaitException {
         try {
             return CommandType.valueOf(input);
