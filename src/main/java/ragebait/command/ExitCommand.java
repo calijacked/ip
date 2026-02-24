7ade4795382e45f7e146940c6b79e1a43e60de0f
calijacked <162989170+calijacked@users.noreply.github.com>
2026-02-24 22:14:22 +0800

    Improve code quality for ExitCommand

--- src/main/java/ragebait/command/ExitCommand.java
+++ src/main/java/ragebait/command/ExitCommand.java
@@ -6,18 +6,24 @@ import ragebait.task.TaskList;
 import ragebait.ui.UI;
 
 /**
- * Represents a command that exits the Ragebait application.
- * Saves the current tasks before terminating the program.
+ * Represents the command to exit the Ragebait application.
+ *
+ * When executed, this command saves the current state of tasks
+ * to persistent storage and returns a farewell message for the user.
  */
 public class ExitCommand extends Command {
 
     /**
-     * Executes the exit command by saving the current TaskList
-     * and displaying a farewell message.
+     * Executes the exit command.
      *
-     * @param tasks The TaskList containing all tasks.
-     * @param ui The UI used to display messages.
+     * This method saves the current tasks to the provided Storage instance
+     * and returns a goodbye message obtained from the UI.
+     *
+     * @param tasks   The TaskList containing all tasks to be saved.
+     * @param ui      The UI used to generate a farewell message.
      * @param storage The Storage responsible for persisting tasks.
+     * @return A farewell message to display to the user.
+     * @throws RagebaitException If saving tasks to storage fails.
      */
     @Override
     public String execute(TaskList tasks, UI ui, Storage storage) throws RagebaitException {
